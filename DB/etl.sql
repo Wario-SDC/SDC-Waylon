e3b6620f9b13b04af6956bf2fc219ad106e17e53
WaylonMarble <marblewaylon@gmail.com>
2022-03-04 12:00:42 -0700

    db up and running

--- DB/etl.sql
+++ DB/etl.sql
@@ -3,6 +3,16 @@
 \COPY questions FROM 'DB/questions-processed.csv' WITH DELIMITER ',' NULL as ' ' CSV header;
 
 \COPY answers FROM 'DB/answers-processed.csv' WITH DELIMITER ',' NULL as ' ' CSV header;
+ALTER TABLE answers
+ADD CONSTRAINT fk_question
+      FOREIGN KEY(question_id)
+	      REFERENCES questions(question_id)
+ON DELETE CASCADE;
 
 \COPY photos FROM 'DB/answers_photos.csv' WITH DELIMITER ',' NULL as ' ' CSV header;
+ALTER TABLE photos
+ADD CONSTRAINT fk_answer
+      FOREIGN KEY(answer_id)
+	      REFERENCES answers(answer_id)
+ON DELETE CASCADE;
 
--- DB/postgresSchema.sql
+++ DB/postgresSchema.sql
@@ -7,7 +7,7 @@ CREATE DATABASE qna;
 DROP TABLE IF EXISTS questions, answers, photos;
 
 CREATE TABLE questions(
-  question_id serial PRIMARY KEY,
+  question_id BIGSERIAL PRIMARY KEY,
   product_id INT NOT NULL,
   body TEXT NOT NULL,
   time_stamp TIMESTAMPTZ NOT NULL,
@@ -18,24 +18,18 @@ CREATE TABLE questions(
 );
 
 CREATE TABLE answers(
-  answer_id serial PRIMARY KEY,
+  answer_id BIGSERIAL PRIMARY KEY,
   question_id INT NOT NULL,
   body TEXT NOT NULL,
   time_stamp TIMESTAMPTZ NOT NULL,
   username VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   reported BOOLEAN NOT NULL,
-  helpfull INT NOT NULL,
-  CONSTRAINT fk_question
-      FOREIGN KEY(question_id)
-	      REFERENCES questions(question_id)
+  helpfull INT NOT NULL
 );
 
 CREATE TABLE photos(
-  photo_id serial PRIMARY KEY,
+  photo_id BIGSERIAL PRIMARY KEY,
   answer_id INT NOT NULL,
-  photo_url VARCHAR(150) NOT NULL,
-  CONSTRAINT fk_answer
-      FOREIGN KEY(answer_id)
-	      REFERENCES answers(answer_id)
+  photo_url VARCHAR(150) NOT NULL
 );
